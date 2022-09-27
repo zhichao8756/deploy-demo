b7be2b234f4167153cc298e135b6a1a91ab55ce5
zzc <875690678@qq.com>
2022-09-27 13:40:47 +0800

    feat: 初始化

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+// import HelloWorld from './components/HelloWorld.vue'
+import { BubbleMessage } from 'js-dynamic-island'
+function testBubble() {
+  BubbleMessage({
+    user: 'zack',
+    message: 'ot take to heart every thing you hear. do not take to heart every thing you hear. do not spend all; Whenever you find your wrongdoing',
+    avatar: 'https://joeschmoe.io/api/v1/random',
+    type: 'success'
+  })
+}
+function testFold() {
+  BubbleMessage({
+    message: 'ot take to heart every thing you hear. do not take to heart every thing you hear. do not spend all; Whenever you find your wrongdoing',
+    type: 'success'
+  })
+}
+</script>
+
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <button @click="testBubble">气泡信息</button>
+  <button @click="testFold">折叠信息</button>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import "../node_modules/js-dynamic-island/style.css"; //引入组件样式
+
+createApp(App).mount('#app')
